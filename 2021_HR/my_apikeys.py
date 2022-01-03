0f767319219456d3d1e5ec5a726a943e949225e4
Jehyun Lee <jehyun.lee@gmail.com>
2022-01-03 15:50:23 +0900

    [ADD] DONE

---
+++ 2021_HR/my_apikeys.py
@@ -0,0 +1,8 @@
+APIKeys = ["967ac6c5db7b8b34c178035e0ea7e22c", 
+           "55eef3b02fc28c636e2fada035cf03d2", 
+           "e1fc56d1d9fcc86e6998a7fd79faed23", 
+           "dcbed1c3abb36a8e80addbf185f38671", 
+           "78d8e510bda00c5530f560f02bc051bc", 
+           "4b9ca0308c3ae9b650f74fd19d07d657", 
+           "8b02da5833a012844657fc27d129349f", 
+           "6624310bd1610368c82261bc6d437be4"]
